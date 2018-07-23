0a54e674d127eab530015eefc9ac10125c660b87
Leonardo Monteiro <leonardo.monteiro@gingaone.com>
2018-07-23 12:55:33 -0300

    Final changes

--- config/config.js
+++ config/config.js
@@ -4,20 +4,23 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    seederStorage: 'sequelize'
   }
 }
